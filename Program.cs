20f4e1d12d9a91226b6f0e39895ddb292f3d9935
uchihaakram <115807011+uchihaakram@users.noreply.github.com>
2022-12-29 21:30:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a n1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a n2");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("the prime numbers are:");
+            int i, j;
+
+
+            for (i = n1; i <= n2; i++)
+            {
+                int count = 0;
+                for (j = 1; j <= i; j++)
+                {
+
+                    if (i % j == 0)
+                    { count++; }
+                }
+
+                if (count <= 2)
+                { Console.WriteLine(i); }
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
